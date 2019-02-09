beb98a08e638b3e38a422972e2981ca99e0d8309
preethi152 <47471999+preethi152@users.noreply.github.com>
2019-02-09 15:59:48 +0530

    Update and rename pos.c to positive.c

--- positive.c
+++ positive.c
@@ -6,16 +6,16 @@ int main(void) {
 	scanf("%d",&n);
 	if(n>0)
 	{
-		printf("positive");
+		printf("Positive");
  
 	}
 	else if(n<0)
 	{
-		printf("negative");
+		printf("Negative");
 	}
 	else
 	{
-		printf("zero");
+		printf("Zero");
 	}
 	return 0;
 }
