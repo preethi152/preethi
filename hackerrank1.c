a7082e9a15f1e954a034105cb2cfa6f495114c80
preethi152 <47471999+preethi152@users.noreply.github.com>
2019-02-21 21:17:04 +0530

    Create hackerrank1.c

---
+++ hackerrank1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n,a;
+    char s[10];
+    scanf("%d",&n);
+    scanf("%s",s);
+    a=n*2;
+    printf("%d",a);
+    printf("\n%s",s);
+    return 0;
+    
+}
