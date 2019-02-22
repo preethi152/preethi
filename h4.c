dc12f30e79ec48bafb5e71281c9c2acff6be29e4
preethi152 <47471999+preethi152@users.noreply.github.com>
2019-02-22 22:24:54 +0530

    Create h4.c
    decimal float

---
+++ h4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b,c,d;
+
+    float a1,a2;
+    float e,f;
+    scanf("%d%d",&a,&b);
+    scanf("%f%f",&a1,&a2);
+    c=a+b;
+    d=a-b;
+    printf("%d",c);
+    printf(" %d",d);
+ e=a1+a2;
+ f=a1-a2;
+ printf("\n%.1f",e);
+    printf(" %.1f",f);
+
+
+
+
+    
+    return 0;
+}
